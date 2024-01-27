aea6073584379b9dea51cfaf16c95e62aaa33fa6
Syed Atif Ali <157229119+syedatifkashifali@users.noreply.github.com>
2024-01-27 19:14:34 +0500

    Create user defined functions.py

---
+++ user defined functions.py
@@ -0,0 +1,21 @@
+#Example 1
+import math#including external file which contain pre-defined function related to mathematics
+x = math.sqrt(64) #math is the method and sqrt is the function
+print(x)
+
+#Example 2
+import math#including external file which contain pre-defined function related to mathematics
+x = math.ceil(1.4)#calling ceil function from math method
+y = math.floor(1.4)#calling floor function from math method
+print(x)#printing x
+print(y)#printing y
+
+#Example 3
+import math#including external file which contain pre-defined function related to mathematics
+x = math.pi#calling value of pie from math method
+print(x)#printing pi value in x
+
+#Example 4
+import datetime#including external file which contain pre-defined function related to date and time
+x = datetime.datetime.now()
+print(x)
