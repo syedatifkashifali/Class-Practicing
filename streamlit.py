a49111e86a599b9b6927562ef605583940c4d4c6
Syed Atif Ali <157229119+syedatifkashifali@users.noreply.github.com>
2024-02-17 07:19:52 -0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -94,14 +94,29 @@ if __name__ == "__main__":
 import streamlit as st
 from PIL import Image
 
-# Title
-st.title("Display Image Example")
+import base64
 
-# Load image
-image = Image.background("download.jpg")
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
 
-# Display image 
-st.image(image, caption='Example Image', use_column_width=True)
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
+    <style>
+    body {
+    background-image: url("data:image/png;base64,%s");
+    background-size: cover;
+    }
+    </style>
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('background.png')
 
 
 import  streamlit as st
