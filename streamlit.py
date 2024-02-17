e45dd0e952c75b9eab86412401deb96199e2f39f
Syed Atif Ali <157229119+syedatifkashifali@users.noreply.github.com>
2024-02-17 06:20:59 -0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -24,3 +24,37 @@ i1 = st.button("button 1")
 st.write("value:", i1)
 
 i2 = st.checkbox("reset button")
+
+
+import streamlit as st
+
+st.header('This is a header with a divider', divider='rainbow')
+st.header('_ Sir.Ghufran Kamal_ is :blue[cool] :sunglasses:')
+footer="""<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Developed with ❤ by <a style='display: block; text-align: center;' href="https://banoqabil.pk" target="_blank">Bano Qabil Alkhidmat </a></p>
+</div>
+"""
+st.markdown(footer,unsafe_allow_html=True)
