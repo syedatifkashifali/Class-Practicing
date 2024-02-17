2737868e880e23929e7d96435d5cb9a2ebb85049
Syed Atif Ali <157229119+syedatifkashifali@users.noreply.github.com>
2024-02-17 06:16:32 -0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -16,3 +16,11 @@ agree = st.checkbox('I agree')
 
 if agree:
     st.write('Great!')
+
+
+import streamlit as st
+
+i1 = st.button("button 1")
+st.write("value:", i1)
+
+i2 = st.checkbox("reset button")
