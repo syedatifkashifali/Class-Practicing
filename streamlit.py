43c02bbda11e80d1e4bc65c81ace8c5458f77011
Syed Atif Ali <157229119+syedatifkashifali@users.noreply.github.com>
2024-02-18 06:14:29 -0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -327,3 +327,9 @@ my_bar.empty()
 st.button("Rerun")
 
 
+import datetime
+import streamlit as st
+
+d = st.date_input("When's your birthday", datetime.date(2000, 7, 6))
+st.write('Your birthday is:', d)
+
