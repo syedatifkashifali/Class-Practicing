40542a77ae157fa34210e82212be5040966e1599
Syed Atif Ali <157229119+syedatifkashifali@users.noreply.github.com>
2024-02-18 06:23:28 -0800

    Update more_streamlit_ex.py

--- more_streamlit_ex.py
+++ more_streamlit_ex.py
@@ -3,3 +3,28 @@ import streamlit as st
 
 d = st.date_input("When's your birthday", datetime.date(2000, 7, 6))
 st.write('Your birthday is:', d)
+
+
+import pandas as pd
+import streamlit as st
+
+data_df = pd.DataFrame(
+    {
+        "apps": [
+            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/5435b8cb-6c6c-490b-9608-799b543655d3/Home_Page.png",
+            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/ef9a7627-13f2-47e5-8f65-3f69bb38a5c2/Home_Page.png",
+            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/31b99099-8eae-4ff8-aa89-042895ed3843/Home_Page.png",
+            "https://storage.googleapis.com/s4a-prod-share-preview/default/st_app_screenshot_image/6a399b09-241e-4ae7-a31f-7640dc1d181e/Home_Page.png",
+        ],
+    }
+)
+
+st.data_editor(
+    data_df,
+    column_config={
+        "apps": st.column_config.ImageColumn(
+            "Preview Image", help="Streamlit app preview screenshots"
+        )
+    },
+    hide_index=True,
+)
